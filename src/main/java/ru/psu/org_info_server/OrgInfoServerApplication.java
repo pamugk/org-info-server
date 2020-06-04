21cd59312aeb717bbf786a5b33ea08aae6814335
Alexander Permyakov <pamugk@gmail.com>
2020-06-04 22:51:35 +0500

    Project initialization

---
+++ src/main/java/ru/psu/org_info_server/OrgInfoServerApplication.java
@@ -0,0 +1,13 @@
+package ru.psu.org_info_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgInfoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgInfoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/psu/org_info_server/OrgInfoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.psu.org_info_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgInfoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
